0f4205dbe93190398a344f5b4c6a2e9a7259d3fe
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-12 16:58:18 -0300

    chore: create reset and recover password routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { SignUp } from "./pages/SignUp/SignUp";
 import { SignIn } from "./pages/SignIn/SignIn";
 import { Home } from "./pages/Home/Home";
+import { RecoverPassword } from "./pages/RecoverPassword/RecoverPassword";
+import { ResetPassword } from "./pages/ResetPassword/ResetPassword";
 
 export function Router() {
   return (
@@ -10,6 +12,8 @@ export function Router() {
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/recover-password" element={<RecoverPassword />} />
+        <Route path="/reset-password/:recoverCode" element={<ResetPassword />} />
       </Route>
     </Routes>
   );
